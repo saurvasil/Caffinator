9d31fc6af416466c48938913e926c2aa9fc66b39
Saurabh Vasil <saurvasil@saurvasilmacbook.local>
2018-01-20 20:26:00 -0500

    added FB login

--- Caffinator/Controller/AppDelegate.swift
+++ Caffinator/Controller/AppDelegate.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,12 +22,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        FBSDKAppEvents.activateApp()
+
+    }
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -36,6 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
--- Caffinator/View/Auth/SignUpViewController.swift
+++ Caffinator/View/Auth/SignUpViewController.swift
@@ -9,21 +9,67 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FBSDKLoginKit
+import FacebookLogin
 
 class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    var dict : [String : AnyObject]!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround() 
+        self.hideKeyboardWhenTappedAround()
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func btnFacebookLogic(_ sender: UIButton) {
+            let loginManager = LoginManager()
+            loginManager.logOut()
+        loginManager.logIn(readPermissions: [ .publicProfile, .email ], viewController: self) { loginResult in
+                switch loginResult {
+                case .failed(let error):
+                    print(error)
+                    
+                case .cancelled:
+                    print("User cancelled login.")
+                    
+                case .success(let grantedPermissions, _ , _):
+                    if (FBSDKAccessToken.current()?.userID != nil) {
+                        if grantedPermissions.contains("email") {
+                            
+                            let graphRequest:FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"first_name,last_name, email, picture.type(large),id,name,hometown,locale,timezone,gender,birthday"])
+                            graphRequest.start(completionHandler: { (connection, result, error) in
+                                if (error != nil) {
+                                    print("Error \(String(describing: error))")
+                                    
+                                } else {
+                                    print("values received from FB are : \(String(describing: result))")
+                                    if let values = result as? NSDictionary {
+                                        let email = values.object(forKey: "email") as? NSString
+                                        let gender = values.object(forKey: "gender")
+                                        let userName = values.object(forKey: "name")
+                                        let socialId = values.object(forKey: "id") as! NSString
+                                        
+                                        //here we should go to next view controller with information prefilled.
+                                        
+                                        
+                                       // print("Email id == \(String(describing: email)) Gender == \(String(describing: gender)) UserName == \(String(describing: userName)) SocialId == \(socialId)")
+                                    }
+                                }
+                            })
+                            }
+                        }
+                    }
+                }
+            }
+    
     
     @IBAction func back(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
-    
     
     
     @IBAction func createAccount(_ sender: UIButton) {
